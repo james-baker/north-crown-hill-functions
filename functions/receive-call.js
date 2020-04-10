9afd78d422cc31fe75d3f71e747613e1857e3b05
James Baker <james-baker@users.noreply.github.com>
2020-04-10 14:31:14 -0700

    start building (WIP)

--- functions/receive-call.js
+++ functions/receive-call.js
@@ -1,3 +1,4 @@
+// https://nch-functions.netlify.com/.netlify/functions/receive-call
 require('dotenv').config();
 
 const {
@@ -5,6 +6,10 @@ const {
   TWILIO_AUTH_TOKEN
 } = process.env;
 
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+
 exports.handler = function(event, context, callback) {
+  const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
   console.log('Hello world!')
+  callback(null, { statusCode: 200, body: 'Received request' })
 };
