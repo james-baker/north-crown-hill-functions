0926f3b5dff82f65214c0e174c447e0d503f38df
James Baker <james-baker@users.noreply.github.com>
2020-04-10 13:59:12 -0700

    install twilio

--- functions/receive-call.js
+++ functions/receive-call.js
@@ -1,3 +1,10 @@
+require('dotenv').config();
+
+const {
+  TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN
+} = process.env;
+
 exports.handler = function(event, context, callback) {
   console.log('Hello world!')
 };
