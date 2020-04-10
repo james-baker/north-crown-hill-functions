c3c6b54e8f9ca03153528bc5a5f0da7d4852fa8b
James Baker <james-baker@users.noreply.github.com>
2020-04-10 15:38:51 -0700

    switch to async function form

--- functions/receive-call.js
+++ functions/receive-call.js
@@ -8,8 +8,13 @@ const {
 
 const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
-exports.handler = function(event, context, callback) {
+exports.handler = async (event, context) => {
   const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-  console.log('Hello world!')
-  callback(null, { statusCode: 200, body: 'Received request' })
+  console.log('Hello async world!');
+  console.log("Event: "+JSON.stringify(event));
+  console.log("Context: "+JSON.stringify(context));
+  return {
+    statusCode: 200,
+    body: "Received request"
+  };
 };
