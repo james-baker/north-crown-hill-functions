6aaeedad3575c8c8be36c5d3ae2d28a5724286da
James Baker <james-baker@users.noreply.github.com>
2020-04-10 22:29:50 -0700

    just mucking about

--- functions/receive-call.js
+++ functions/receive-call.js
@@ -1,18 +1,40 @@
 // https://nch-functions.netlify.com/.netlify/functions/receive-call
-require('dotenv').config();
+require("dotenv").config();
+import querystring from "querystring";
+//import fetch from "node-fetch";
 
-const {
-  TWILIO_ACCOUNT_SID,
-  TWILIO_AUTH_TOKEN
-} = process.env;
-
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const VoiceResponse = require("twilio").twiml.VoiceResponse;
 
 exports.handler = async (event, context) => {
-  const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-  console.log('Hello async world!');
+  const client = require("twilio")(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+  console.log("Hello async world!");
+
+  var params;
+  if (event.httpMethod === "POST") {
+    params = querystring.parse(event.body); //POST puts params in the event body encoded as a query string
+  } else {
+    params = event.queryStringParameters;
+  }
+
+  // // Send greeting to Slack
+  // return fetch(process.env.SLACK_WEBHOOK_URL, {
+  //   headers: {
+  //     "content-type": "application/json",
+  //   },
+  //   method: "POST",
+  //   body: JSON.stringify({ text: `${name} says hello!` }),
+  // })
+  //   .then(() => ({
+  //     statusCode: 200,
+  //     body: `Hello, ${name}! Your greeting has been sent to Slack 👋`,
+  //   }))
+  //   .catch((error) => ({
+  //     statusCode: 422,
+  //     body: `Oops! Something went wrong. ${error}`,
+  //   }));
+
   return {
     statusCode: 200,
-    body: "Received request<br/>Event: "+JSON.stringify(event)+"<br/>Context: "+JSON.stringify(context)
+    body: "Received request<br/>Event: " + JSON.stringify(params),
   };
 };
