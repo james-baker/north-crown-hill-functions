056f6f7c87f89bfb6fc764b2f6f647c606f538d2
James Baker <james-baker@users.noreply.github.com>
2020-04-16 15:58:31 -0700

    this would work if conversations.history returned "reply"s and not just "message"s

--- functions/receive-recording.js
+++ functions/receive-recording.js
@@ -7,16 +7,30 @@ const slack = require("../lib/slack-wrappers");
 
 exports.handler = async (event, context) => {
   console.log(`Running receive-recording as ${event.httpMethod}`);
+  const channel = "#bot-testing";
 
   const params = qs.getParams(event);
   console.log(params);
   if (!params || !params.CallSid) {
-    await slack.postMessage("#bot-testing", `receive-recording error, missing params or CallSid: ${JSON.stringify(params)}`);
+    await slack.postMessage(channel, `receive-recording error, missing params or CallSid: ${JSON.stringify(params)}`);
   } else if (!params.RecordingUrl) {
-    await slack.postMessage("#bot-testing", `receive-recording error for call ${params.CallSid}, params did not contain RecordingUrl`);
+    await slack.postMessage(channel, `receive-recording error for call ${params.CallSid}, params did not contain RecordingUrl`);
   } else {
-    const recording = params.RecordingUrl + ".mp3";
-    await slack.postMessage("#bot-testing", `Recorded voicemail (${params.RecordingDuration} seconds) for call ID ${params.CallSid}: ${recording}`);
+    const searchResult = await slack.search(params.CallSid);
+    console.log(searchResult);
+    if (searchResult && searchResult.ok === true && searchResult.messages && searchResult.messages.total === 1) {
+      console.log(searchResult.messages.matches[0]);
+      const match = searchResult.messages.matches[0];
+      const matchChannel = match.channel.id;
+      console.log(matchChannel);
+      const matchUser = match.username; //nchbot
+      const matchTS = match.ts;
+      const messageResult = await slack.getMessage(matchChannel, matchTS);
+      console.log(messageResult);
+    
+      const recording = params.RecordingUrl + ".mp3";
+      await slack.postReply(channel, `Recorded voicemail (${params.RecordingDuration} seconds) for call ID ${params.CallSid}: ${recording}`, messageResult.messages[0].thread_ts);
+    }
   }
 
   // const response = new VoiceResponse();
